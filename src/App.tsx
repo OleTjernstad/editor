b2845bfbef31c9487a0ea33f1472edf8f23f0c73
Ole Tjernstad <ott556@gmail.com>
2023-05-22 22:35:37 +0200

    add image block

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { useEffect, useRef, useState } from "react";
 
+import AddIcon from "@mui/icons-material/Add";
 import { Box } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import FormControl from "@mui/material/FormControl";
+import GrainIcon from "@mui/icons-material/Grain";
 import IconButton from "@mui/material/IconButton";
+import ImageIcon from "@mui/icons-material/Image";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
 import TextField from "@mui/material/TextField";
 
 const regex = /(?<paragraph>.+)(\r?\n)?/gm;
 
+type BlockType =
+  | "paragraph"
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "h6"
+  | "image";
 interface Block {
   key: string;
   text: string;
-  type: "paragraph" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  type: BlockType;
 }
 type Content = Block[];
 
@@ -44,8 +62,19 @@ function App() {
       });
     });
   }
+  function updateBlockType(key: string | undefined, type: BlockType) {
+    if (key === undefined) {
+      addModule(isFocused.current, type);
+    }
+    setContent((prev) => {
+      return prev.map((block) => {
+        if (block.key === key) return { ...block, type };
+        return block;
+      });
+    });
+  }
 
-  function addModule(keyBefore: string) {
+  function addModule(keyBefore: string | undefined, type?: BlockType) {
     const createdKey = createId();
     console.log(createdKey);
 
@@ -57,7 +86,7 @@ function App() {
       content.splice(index + 1, 0, {
         key: createdKey,
         text: "",
-        type: "paragraph",
+        type: type ?? "paragraph",
       });
       return content;
     });
@@ -80,6 +109,12 @@ function App() {
 
   return (
     <div>
+      <BlockDialog
+        isActive
+        updateBlockType={updateBlockType}
+        deleteBlock={removeBlock}
+        icon={<AddIcon />}
+      />
       {content.map((c) => {
         switch (c.type) {
           case "h1":
@@ -98,6 +133,7 @@ function App() {
                 removeBlock={removeBlock}
                 resetFocus={resetFocus}
                 updateFocus={updateFocus}
+                updateBlockType={updateBlockType}
               />
             );
 
@@ -112,6 +148,7 @@ function App() {
                 removeBlock={removeBlock}
                 resetFocus={resetFocus}
                 updateFocus={updateFocus}
+                updateBlockType={updateBlockType}
               />
             );
         }
@@ -122,7 +159,7 @@ function App() {
 
 export default App;
 
-interface ParagraphFieldProps {
+interface FieldProps {
   block: Block;
   update: (key: string, text: string) => void;
   addModule: (keyBefore: string) => void;
@@ -130,7 +167,10 @@ interface ParagraphFieldProps {
   updateFocus: (key: string) => void;
   removeBlock: (key: string) => void;
   resetFocus: string | undefined;
+  updateBlockType: (key: string | undefined, type: BlockType) => void;
 }
+
+type ParagraphFieldProps = FieldProps;
 function ParagraphField({
   block,
   update,
@@ -139,6 +179,7 @@ function ParagraphField({
   removeBlock,
   resetFocus,
   updateFocus,
+  updateBlockType,
 }: ParagraphFieldProps) {
   const preventChange = useRef<boolean>();
   const inputElement = useRef<HTMLTextAreaElement>(null);
@@ -177,13 +218,13 @@ function ParagraphField({
 
   return (
     <Box sx={{ display: "flex", flexDirection: "row" }}>
-      <IconButton
-        aria-label="delete"
-        disabled={resetFocus !== block.key}
-        onClick={() => inputElement.current?.focus()}
-      >
-        <DeleteIcon />
-      </IconButton>
+      <BlockDialog
+        block={block}
+        isActive={resetFocus === block.key}
+        updateBlockType={updateBlockType}
+        deleteBlock={removeBlock}
+        icon={<GrainIcon />}
+      />
       <TextField
         inputRef={inputElement}
         id={block.key}
@@ -207,15 +248,7 @@ function ParagraphField({
   );
 }
 
-interface TitleFieldProps {
-  block: Block;
-  update: (key: string, text: string) => void;
-  addModule: (keyBefore: string) => void;
-  isFocused: string | undefined;
-  updateFocus: (key: string) => void;
-  removeBlock: (key: string) => void;
-  resetFocus: string | undefined;
-}
+type TitleFieldProps = FieldProps;
 function TitleField({
   block,
   update,
@@ -224,6 +257,7 @@ function TitleField({
   removeBlock,
   resetFocus,
   updateFocus,
+  updateBlockType,
 }: TitleFieldProps) {
   const inputElement = useRef<HTMLTextAreaElement>(null);
 
@@ -256,18 +290,17 @@ function TitleField({
 
   return (
     <Box sx={{ display: "flex", flexDirection: "row" }}>
-      <IconButton
-        aria-label="delete"
-        disabled={resetFocus !== block.key}
-        onClick={() => inputElement.current?.focus()}
-      >
-        <DeleteIcon />
-      </IconButton>
+      <BlockDialog
+        block={block}
+        isActive={resetFocus === block.key}
+        updateBlockType={updateBlockType}
+        deleteBlock={removeBlock}
+        icon={<GrainIcon />}
+      />
       <TextField
         inputRef={inputElement}
         id={block.key}
         value={block.text}
-        minRows={2}
         fullWidth
         variant="standard"
         InputProps={{
@@ -303,4 +336,102 @@ function headerStyle(header: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | string) {
     default:
       break;
   }
+}
+
+interface SelectBlockTypeProps {
+  isActive: boolean;
+  block?: Block;
+  updateBlockType: (key: string | undefined, type: BlockType) => void;
+}
+function SelectBlockType({
+  isActive,
+  block,
+  updateBlockType,
+}: SelectBlockTypeProps) {
+  const handleChange = (event: SelectChangeEvent) => {
+    if (block) updateBlockType(block.key, event.target.value as BlockType);
+  };
+
+  return (
+    <FormControl
+      sx={{ mt: 2, minWidth: 120 }}
+      disabled={!isActive}
+      size="small"
+      fullWidth
+    >
+      <InputLabel id="demo-simple-select-label">Tekst</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={block?.type}
+        label="Age"
+        onChange={handleChange}
+      >
+        <MenuItem value={"paragraph"}>Tekst</MenuItem>
+        <MenuItem value={"h1"}>Tittel 1</MenuItem>
+        <MenuItem value={"h2"}>Tittel 2</MenuItem>
+        <MenuItem value={"h3"}>Tittel 3</MenuItem>
+        <MenuItem value={"h4"}>Tittel 4</MenuItem>
+        <MenuItem value={"h5"}>Tittel 5</MenuItem>
+        <MenuItem value={"h6"}>Tittel 6</MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
+
+type BlockDialogProps = SelectBlockTypeProps & {
+  deleteBlock: (key: string) => void;
+  icon: React.ReactNode;
+};
+function BlockDialog(props: BlockDialogProps) {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <IconButton
+        aria-label="delete"
+        disabled={!props.isActive}
+        onClick={handleClickOpen}
+        sx={{
+          "&.Mui-disabled": {
+            background: "transparent",
+            color: "transparent",
+          },
+        }}
+      >
+        {props.icon}
+      </IconButton>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogContent>
+          <SelectBlockType {...props} />
+          {props?.block?.key ? (
+            <IconButton
+              aria-label="delete"
+              disabled={!props.isActive}
+              onClick={() => props.deleteBlock(props?.block?.key ?? "")}
+            >
+              <DeleteIcon />
+            </IconButton>
+          ) : null}
+          {props?.block?.type === "image" || props?.block === undefined ? (
+            <IconButton
+              aria-label="add image"
+              disabled={!props.isActive}
+              onClick={() => props.updateBlockType(undefined, "image")}
+            >
+              <ImageIcon />
+            </IconButton>
+          ) : null}
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 }
